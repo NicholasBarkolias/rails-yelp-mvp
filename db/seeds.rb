a111faeb1a45e77049ebc67bc26656feb8352db6
Nicholas Barkolias <nbarkolias@gmail.com>
2020-08-25 21:40:35 +1000

    backend done

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,13 +1,13 @@
 puts 'Cleaning database'
 
+Restaurant.destroy_all
 10.times do
-  Restaurant.destroy_all
-
   puts 'Creating restaurants'
   Restaurant.create({
                       name: Faker::Restaurant.name,
                       address: Faker::Address.street_address,
-                      category: ["chinese", "italian", "japanese", "french", "belgian"].sample
+                      category: ["chinese", "italian", "japanese", "french", "belgian"].sample,
+                      phone_number: Faker::PhoneNumber.phone_number
                     })
 end
 puts 'Finished'
